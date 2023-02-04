a10f98af65d2c1b3bea7abda039249b2af79cfb0
youabledev <youable.framios@gmail.com>
2023-02-04 21:54:04 +0900

    ADD: SceneDelegate Setting & Singleton

---
+++ Archive/Basic Setting/Singleton.swift
@@ -0,0 +1,12 @@
+//
+//  Singleton.swift
+//  Archive
+//
+//  Created by ZUMIN YOU on 2023/02/04.
+//
+
+import Foundation
+
+class Singleton {
+    static var sharedObject: Singleton = Singleton()
+}
